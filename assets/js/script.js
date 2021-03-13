500e559a1e402b4d5566879c06543c4b1eb6f4c9
Carlos Vadillo <cavarod@gmail.com>
2021-03-13 07:13:46 -0800

    generated the response.html to play with wine/recipe display, card is looking good but the collapse section needs work

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,14 +3,18 @@
 
 
 
- // Initializing the DOM Form functions for Materialize (DROP DOWNS ETC)
+ // Initializing the DOM Form functions for Materialize (DROP DOWNS)
+ // THERE IS A VARIABLE BUG WITH THE VANILLA JS, JQUERY ADDED
 document.addEventListener('DOMContentLoaded', function() {
-	var elems = document.querySelectorAll('select');
-	var instances = M.FormSelect.init(elems, options);
+	var selectElems = document.querySelectorAll('select');
+	var selectInstances = M.FormSelect.init(selectElems, options);
+	var collapseElems = document.querySelectorAll('.collapsible');
+    var collapseInstances = M.Collapsible.init(collapseElems, options);
 });
 
 // Initializing the DOM Form functions for Materialize (JQuery)
 $(document).ready(function(){
 	$('select').formSelect();
+	$('.collapsible').collapsible()
 });
         
