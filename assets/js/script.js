4741f1970d445667b5729e0514ff7c59dd3dc913
Tim Weyel <tweyel@gmail.com>
2021-03-14 14:02:12 -0700

    added foodCardGenerator to dynamically generate results page

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,9 @@
 var wineSelectorEl = document.querySelector('#wineSelector'); 
 var mainEl = document.querySelector('#main');
 var headerEl = document.querySelector('#header');
-
+var selectedWineOption = wineSelectorEl.options[wineSelectorEl.selectedIndex].text;
+var selectedOptionVal = wineSelectorEl.value;
+console.log(selectedWineOption);
 
  // Initializing the DOM Form functions for Materialize (DROP DOWNS)
  // THERE IS A VARIABLE BUG WITH THE VANILLA JS, JQUERY ADDED
@@ -26,9 +28,77 @@ var removeExistingElems = function() {
 	mainEl.innerHTML = '';
 }
 
-wineSelectorEl.addEventListener('change', removeExistingElems);
+wineSelectorEl.addEventListener('change', function() {
+	removeExistingElems();
+	foodCardGenerator();
 
-//create a function to generate the the food card, description
+});
 
+//create a function to generate the food card and description
+var foodCardGenerator = function() {
+	var foodCardContainer = document.createElement('div');
+	foodCardContainer.innerHTML = `<div class="container">
+	<div class="row">
+			<div class="col s12 m5">
+					<div class="row">
+							<div class="col s12">
+									<div class="card">
+											<div class="card-image">
+													<img src="./assets/images/sample-1.jpg">
+													<span class="card-title">` + selectedWineOption + `</span>
+											</div>
+											<div class="card-content">
+													<div class ="row">
+															<div class="col s6">
+																	<h4>I will be the Info</h4>
+																	<p>I am a very simple card. I am good at containing small bits of information.
+																	I am convenient because I require little markup to use effectively.</p>
+															</div>
+															<div class="col s6">
+																	<h4>I will be the Rating</h4>
+																	<p>I am a very simple card. I am good at containing small bits of information.
+																	I am convenient because I require little markup to use effectively.</p>
+															</div>
+													</div>
+											</div>
+											<div class="card-action">
+													<a href="#">This is a link</a>
+											</div>
+									</div>
+							</div>
+					</div>
+			</div>
+			<div class="col s12 m7">
+					<ul class="collapsible">
+							<li>
+									<div class="collapsible-header">
+											<i class="material-icons">filter_drama</i>First
+									</div>
+									<div class="collapsible-body">
+											<span>Lorem ipsum dolor sit amet.</span>
+									</div>
+							</li>
+							<li>
+									<div class="collapsible-header">
+											<i class="material-icons">place</i>Second
+									</div>
+									<div class="collapsible-body">
+											<span>Lorem ipsum dolor sit amet.</span>
+									</div>
+							</li>
+							<li>
+									<div class="collapsible-header">
+											<i class="material-icons">whatshot</i>Third
+									</div>
+									<div class="collapsible-body">
+											<span>Lorem ipsum dolor sit amet.</span>
+									</div>
+							</li>
+					</ul>
+			</div>
+	</div>
+</div>`;
+	mainEl.appendChild(foodCardContainer);
+}
 
 //create something to display the 3 options to the right
